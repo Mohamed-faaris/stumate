9562281205abe665c0543fdf4dfeccc9017dd1db
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-10-27 22:28:41 +0530

    refactor queries

---
+++ src/server/db/queries/user.ts
@@ -0,0 +1,23 @@
+import { db } from "../";
+import { type UserRole, users } from "../schema";
+
+export async function insertUser(user: {
+	name: string;
+	role: UserRole;
+	email: string;
+	passwordHash: string;
+	image?: string;
+}) {
+	const [inserted] = await db
+		.insert(users)
+		.values(user)
+		.returning({ id: users.id });
+
+	if (!inserted) {
+		throw new Error("Failed to insert user");
+	}
+
+	return {
+		id: inserted.id,
+	};
+}

7b5c6d48227791dc18bedef72899c7fb9dd33fca
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-10-27 14:24:14 +0530

    schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,7 @@
 import { relations, sql } from "drizzle-orm";
 import { index, pgEnum, pgTableCreator, primaryKey } from "drizzle-orm/pg-core";
 import type { AdapterAccount } from "next-auth/adapters";
+import { required } from "zod/v4-mini";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -10,37 +11,70 @@ import type { AdapterAccount } from "next-auth/adapters";
  */
 export const createTable = pgTableCreator((name) => `stumate_${name}`);
 
-export const posts = createTable(
-	"post",
-	(d) => ({
-		id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-		name: d.varchar({ length: 256 }),
-		createdById: d
-			.varchar({ length: 255 })
-			.notNull()
-			.references(() => users.id),
-		createdAt: d
-			.timestamp({ withTimezone: true })
-			.default(sql`CURRENT_TIMESTAMP`)
-			.notNull(),
-		updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
-	}),
-	(t) => [
-		index("created_by_idx").on(t.createdById),
-		index("name_idx").on(t.name),
-	],
-);
+export const forms = createTable("form", (d) => ({
+	id: d.uuid().notNull().primaryKey().defaultRandom(),
+	title: d.varchar({ length: 255 }).notNull(),
+	description: d.text().notNull(),
+	config: d.jsonb().notNull(),
+	createdBy: d
+		.uuid()
+		.notNull()
+		.references(() => users.id),
+	createdAt: d.timestamp().defaultNow(),
+	updatedAt: d.timestamp().defaultNow(),
+}));
+
+export const formSections = createTable("form_section", (d) => ({
+	id: d.uuid().notNull().primaryKey().defaultRandom(),
+	formId: d
+		.uuid()
+		.notNull()
+		.references(() => forms.id),
+	title: d.varchar({ length: 255 }).notNull(),
+	description: d.text().notNull(),
+	config: d.jsonb().notNull(),
+	order: d.integer().notNull(),
+}));
+
+export const formQuestions = createTable("form_question", (d) => ({
+	id: d.uuid().notNull().primaryKey().defaultRandom(),
+	sectionId: d
+		.uuid()
+		.notNull()
+		.references(() => formSections.id),
+	questionText: d.text().notNull(),
+	questionType: d.varchar({ length: 100 }).notNull(),
+	required: d.boolean().notNull().default(false),
+	config: d.jsonb().notNull(),
+	order: d.integer().notNull(),
+}));
+
+export const formResponsesLog = createTable("form_response_log", (d) => ({
+	id: d.uuid().notNull().primaryKey().defaultRandom(),
+	formId: d
+		.uuid()
+		.notNull()
+		.references(() => forms.id),
+	submittedAt: d.timestamp().defaultNow(),
+	updatedAt: d.timestamp().defaultNow(),
+	responderId: d.uuid().references(() => users.id),
+}));
+
+export const formResponses = createTable("form_response", (d) => ({
+	responseLogId: d
+		.uuid()
+		.notNull()
+		.references(() => formResponsesLog.id)
+		.primaryKey(),
+	answer: d.jsonb().notNull(),
+}));
 
 const rolesValues = ["USER", "ADMIN", "DEV"] as const;
 export const roles = pgEnum("user_role", rolesValues);
 export type UserRole = (typeof rolesValues)[number];
 
 export const users = createTable("user", (d) => ({
-	id: d
-		.varchar({ length: 255 })
-		.notNull()
-		.primaryKey()
-		.$defaultFn(() => crypto.randomUUID()),
+	id: d.uuid().notNull().primaryKey().defaultRandom(),
 	name: d.varchar({ length: 255 }),
 	role: roles("role").default("USER").notNull(),
 	passwordHash: d.char({ length: 60 }),
@@ -52,6 +86,22 @@ export const users = createTable("user", (d) => ({
 	image: d.varchar({ length: 255 }),
 }));
 
+export const usersMetadata = createTable("user_metadata", (d) => ({
+	userId: d
+		.uuid()
+		.notNull()
+		.primaryKey()
+		.references(() => users.id),
+	birthOfdate: d.date(),
+	phoneNumber: d.varchar({ length: 20 }),
+	address: d.text(),
+	defaultLanguage: d.varchar({ length: 10 }).default("en"),
+	college: d.varchar({ length: 255 }),
+	department: d.varchar({ length: 255 }),
+	yearOfStudy: d.integer(),
+	details: d.jsonb(),
+}));
+
 export const usersRelations = relations(users, ({ many }) => ({
 	accounts: many(accounts),
 }));
@@ -60,7 +110,7 @@ export const accounts = createTable(
 	"account",
 	(d) => ({
 		userId: d
-			.varchar({ length: 255 })
+			.uuid()
 			.notNull()
 			.references(() => users.id),
 		type: d.varchar({ length: 255 }).$type<AdapterAccount["type"]>().notNull(),
@@ -89,7 +139,7 @@ export const sessions = createTable(
 	(d) => ({
 		sessionToken: d.varchar({ length: 255 }).notNull().primaryKey(),
 		userId: d
-			.varchar({ length: 255 })
+			.uuid()
 			.notNull()
 			.references(() => users.id),
 		expires: d.timestamp({ mode: "date", withTimezone: true }).notNull(),

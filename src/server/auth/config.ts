f68e792e492e24e118b0e8de0577d06249a329a3
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-11-05 14:54:49 +0530

    custom password function

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
@@ -10,14 +11,22 @@ export const auth = betterAuth({
 	}),
 	secret: env.AUTH_SECRET,
 	trustedOrigins: ["http://localhost:3000"],
-	socialProviders: {
-		discord: {
-			clientId: env.AUTH_DISCORD_ID || "",
-			clientSecret: env.AUTH_DISCORD_SECRET || "",
-		},
-	},
+	// socialProviders: {
+	// 	discord: {
+	// 		clientId: env.AUTH_DISCORD_ID || "",
+	// 		clientSecret: env.AUTH_DISCORD_SECRET || "",
+	// 	},
+	// },
 	emailAndPassword: {
 		enabled: true,
-		autoSignUpEnabled: false,
+		autoSignUpEnabled: true,
+		password: {
+			hash: async (password) => {
+				return await bcrypt.hash(password, 10);
+			},
+			verify: async ({ hash, password }) => {
+				return await bcrypt.compare(password, hash);
+			},
+		},
 	},
 });
